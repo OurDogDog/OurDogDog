1dfdb09b6f7de7c810ce49a1d113748af9af0bb7
pmh5574 <g_a_m_e_@naver.com>
2023-08-02 18:03:15 +0900

    first commit

---
+++ src/main/java/com/dog/meong/MeongApplication.java
@@ -0,0 +1,13 @@
+package com.dog.meong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dog/meong/MeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dog.meong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
